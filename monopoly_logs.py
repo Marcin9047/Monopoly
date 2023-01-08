f46b3eccbe9742ea388d0c08fbc79b4020d1d9ec
Marcin Połosak <marcin.polosak@wp.pl>
2023-01-08 15:05:20 +0100

    basic upgrade of file reading

--- monopoly_logs.py
+++ monopoly_logs.py
@@ -6,18 +6,30 @@ from monopoly import Squere, Property, Area, Player, Dices, dice_throw
 def read_database(database_file):
     with open(database_file, "r") as file_holder:
         data = json.load(file_holder)
-        for number, properties in enumerate(data):
+        for area in data:
             list_of_properties = []
-            for one in properties:
-                name, position, price, rent, area = one
-                name = Property(position, price, rent, area)
-                list_of_properties.append(name)
-            number = Area(list_of_properties)
+            for property in data[area]:
+                row = data[area][property]
+                position = row["position"]
+                price = row["price"]
+                rent = row["rent"]
+                property = Property(position, price, rent, area)
+                list_of_properties.append(property)
+            area = Area(list_of_properties)
 
-def write_game_logs(log_file):
+def write_game_logs(log_file, property):
     with open(log_file, "w") as file_holder:
-        for area in Area:
-            file_holder[area] = [
-            for property in Property:
-                position, price, rent, area, owner, houses = str(property)
-                    ]
+        name, position, price, rent, area, owner, houses = property
+        data = {
+            "name": name,
+            "position": position,
+            "price": price,
+            "rent": rent,
+            "area": area,
+            "owner": owner,
+            "houses": houses
+                }
+        file_holder[Area][property] = data
+
+
+read_database("/home/marcin9047/Programowanie - dom/monopoly/database.json")
